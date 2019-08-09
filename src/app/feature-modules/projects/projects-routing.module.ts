cac8261cc44fc1c91324c1093e1b55abd611bad8
Mauricio Martinez <mauroz77@gmail.com>
2019-08-09 10:40:45 +0100

    Using breadcrumbs to Project, Plan and History pages
    * Add package ng7-bootstrap-breadcrumb ^2.0.0 to have breadcrumbs.

--- src/app/feature-modules/projects/projects-routing.module.ts
+++ src/app/feature-modules/projects/projects-routing.module.ts
@@ -10,15 +10,66 @@ import { AuthGuard } from 'src/app/core/guards/auth.guard';
 import { HistoryComponent } from 'src/app/shared/components/history/history.component';
 
 const routes: Routes = [
-  { path: 'projects/:id', component: ProjectDetailComponent, canActivate: [AuthGuard],
+  {
+    path: 'projects/:id', component: ProjectDetailComponent, canActivate: [AuthGuard],
     // children: [
     //   { path: 'production-plan/:pid', component: ProductionPlanComponent },
     //   { path: 'phenotyping-plan/:pid', component: PhenotypingPlanComponent},
     // ]
   },
-  { path: 'projects/:id/production-plan/:pid', component: ProductionPlanComponent },
-  { path: 'projects/:id/phenotyping-plan/:pid', component: PhenotypingPlanComponent},
-  { path: 'projects/:id/production-plan/:pid/history', component: HistoryComponent},
+  {
+    path: 'projects/:id/production-plan/:pid', component: ProductionPlanComponent,
+    data: {
+      title: 'Production Plan',
+      breadcrumb: [
+        {
+          label: 'Project {{ id }}',
+          url: 'projects/:id'
+        },
+        {
+          label: 'Plan {{ pid }}',
+          url: ''
+        }
+      ]
+    }
+  },
+  {
+    path: 'projects/:id/phenotyping-plan/:pid', component: PhenotypingPlanComponent,
+    data: {
+      title: 'Phenotyping Plan',
+      breadcrumb: [
+        {
+          label: 'Project {{ id }}',
+          url: 'projects/:id'
+        },
+        {
+          label: 'Plan {{ pid }}',
+          url: ''
+        }
+      ]
+    }
+  },
+  {
+    path: 'projects/:id/production-plan/:pid/history', component: HistoryComponent
+    ,data: {
+      title: 'Phenotyping Plan',
+      breadcrumb: [
+        {
+          label: 'Project {{ id }}',
+          url: 'projects/:id'
+         },
+        {
+          label: 'Plan {{ pid }}',
+          url: 'projects/:id/production-plan/:pid'
+        }
+        ,
+        {
+          label: 'History',
+          url: ''
+        }
+      ]
+    }
+  },
   { path: 'projects/:id/phenotyping-plan/:pid/history', component: HistoryComponent },
 
   { path: 'projects', component: ProjectListComponent, canActivate: [AuthGuard] },
