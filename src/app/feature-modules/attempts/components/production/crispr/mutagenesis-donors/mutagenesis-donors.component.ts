abc341cbcfa5187867ffd133f57b6fb4872cf9eb
Mauricio Martinez <mauroz77@gmail.com>
2019-08-13 14:06:07 +0100

    Restructuring code and addition of Phenotyping component
    * Restructuring code to separate production and phenotyping.
    * Initial structure for Phenotyping component.
    * Added plan type to plan details.

--- src/app/feature-modules/attempts/components/production/crispr/mutagenesis-donors/mutagenesis-donors.component.ts
+++ src/app/feature-modules/attempts/components/production/crispr/mutagenesis-donors/mutagenesis-donors.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { CrisprAttempt } from '../../..';
+import { CrisprAttempt } from '../../../..';
 
 @Component({
   selector: 'app-mutagenesis-donors',
