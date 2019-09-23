fe0d561d392e02ebb5515fb120ecd6353dc86ef6
rwilson-ebi <rwilson@ebi.ac.uk>
2019-09-23 12:06:49 +0100

    Updated work-group.service.ts to remove envirnoment.baseUrl and use config.json from the directory assets/config/.

--- src/app/core/services/work-group.service.ts
+++ src/app/core/services/work-group.service.ts
@@ -2,19 +2,24 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { WorkGroup } from '../model/organisation/work-group';
 import { environment } from 'src/environments/environment';
+import {ConfigAssetLoaderService} from './config-asset-loader.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WorkGroupService {
 
-  constructor(private http: HttpClient) { }
+  private url;
+
+  constructor(private http: HttpClient, private configAssetLoaderService: ConfigAssetLoaderService) {
+    this.configAssetLoaderService.loadConfigurations().subscribe(data => this.url = data.appServerUrl);
+  }
 
   getAllWorkGroups() {
-    return this.http.get<WorkGroup[]>(`${environment.baseUrl}/tracking-api/workGroups`);
+    return this.http.get<WorkGroup[]>(this.url + '/tracking-api/workGroups');
   }
 
   getWorkGroupByWorkUnit(name: string) {
-    return this.http.get<WorkGroup[]>(`${environment.baseUrl}/api/workGroups?workUnitName=${name}`);
+    return this.http.get<WorkGroup[]>(this.url + '/api/workGroups?workUnitName=' + name );
   }
 }
