76972f84287c84137a4f88b371d9073a3079d091
rwilson-ebi <rwilson@ebi.ac.uk>
2019-09-23 11:59:36 +0100

    Updated funder.service.ts to remove envirnoment.baseUrl and use config.json from the directory assets/config/.

--- src/app/core/services/funder.service.ts
+++ src/app/core/services/funder.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import {ConfigAssetLoaderService} from './config-asset-loader.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FunderService {
 
-  constructor(private http: HttpClient) { }
+  private url;
+
+  constructor(private http: HttpClient, private configAssetLoaderService: ConfigAssetLoaderService) {
+    this.configAssetLoaderService.loadConfigurations().subscribe(data => this.url = data.appServerUrl);
+  }
 
   getAllFundersByWorkGroup(name: string) {
-    return this.http.get<any[]>(`${environment.baseUrl}/api/funders?workGroupName=${name}`);
+    return this.http.get<any[]>(this.url + '/api/funders?workGroupName=' + name );
   }
 }
