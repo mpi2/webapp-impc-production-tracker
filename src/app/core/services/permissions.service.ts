f630127f2640fbaccf126259acc23e4894937a22
rwilson-ebi <rwilson@ebi.ac.uk>
2019-09-23 12:04:13 +0100

    Updated permissions.service.ts to remove envirnoment.baseUrl and use config.json from the directory assets/config/.

--- src/app/core/services/permissions.service.ts
+++ src/app/core/services/permissions.service.ts
@@ -4,11 +4,14 @@ import { Observable } from 'rxjs';
 import { Permission } from '../model/conf/permission';
 import { environment } from 'src/environments/environment';
 import { map } from 'rxjs/operators';
+import {ConfigAssetLoaderService} from './config-asset-loader.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PermissionsService {
+
+  private url;
 
   // Paths
   static readonly REGISTER_USER = 'register-user';
@@ -18,16 +21,18 @@ export class PermissionsService {
   static readonly UPDATE_PLAN_ACTION = 'canUpdatePlan';
   static readonly UPDATE_PROJECT_ACTION = 'canUpdateProject';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private configAssetLoaderService: ConfigAssetLoaderService) {
+    this.configAssetLoaderService.loadConfigurations().subscribe(data => this.url = data.appServerUrl);
+  }
 
   // Returns an object with permissions for the logged user.
   getPermissions() {
-    return this.http.get<Permission>(`${environment.baseUrl}/api/permissions`);
+    return this.http.get<Permission>(this.url + '/api/permissions');
   }
 
   // Returns if an action over an object is allowed.
   getPermissionByActionOnResource(action: string, resourceId: string) {
-    return this.http.get<boolean>(`${environment.baseUrl}/api/permissionByActionOnResource?action=${action}&resourceId=${resourceId}`);
+    return this.http.get<boolean>(this.url + '/api/permissionByActionOnResource?action=' + action + '&resourceId=' + resourceId );
   }
 
   evaluatePermission(path: string): Observable<boolean> {
