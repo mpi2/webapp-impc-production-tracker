024a07098f490812e7b2a43b752c3a52b197eb91
rwilson-ebi <rwilson@ebi.ac.uk>
2019-09-23 12:14:08 +0100

    Updated user.service.ts to remove envirnoment.baseUrl and use config.json from the directory assets/config/.

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -3,16 +3,21 @@ import { User } from '../model/user/user';
 import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
+import {ConfigAssetLoaderService} from './config-asset-loader.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private http: HttpClient) { }
+  private url;
+
+  constructor(private http: HttpClient, private configAssetLoaderService: ConfigAssetLoaderService) {
+    this.configAssetLoaderService.loadConfigurations().subscribe(data => this.url = data.appServerUrl);
+  }
 
   createUser(user: User) {
-    return this.http.post<User[]>(`${environment.baseUrl}/auth/signup`, user)
+    return this.http.post<User[]>(this.url + '/auth/signup', user)
         .pipe(map(result => {
             // Create user successful if there's a user in the response
             return result;
