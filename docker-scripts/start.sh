b78b4586ca0d1672d2ac57795a5768ca8c9bf06d
rwilson-ebi <rwilson@ebi.ac.uk>
2019-09-24 13:59:57 +0100

    Updated the docker script to use sh rather than bash.

--- docker-scripts/start.sh
+++ docker-scripts/start.sh
@@ -1,12 +1,11 @@
-#!/bin/bash
-set -e
+#!/bin/sh
 
 if [ -n "${API_SERVICE_HOST}" ]; then
-   sed -i 's/127\.0\.0\.1/'"${API_SERVICE_HOST}"'/g' /usr/share/nginx/html/assets/config/config.json
+   /bin/sed -i 's/127\.0\.0\.1/'"${API_SERVICE_HOST}"'/g' /usr/share/nginx/html/assets/config/config.json
 fi
 
 if [ -n "${API_SERVICE_PORT}" ]; then
-   sed -i 's/8080/'"${API_SERVICE_PORT}"'/g' /usr/share/nginx/html/assets/config/config.json
+   /bin/sed -i 's/8080/'"${API_SERVICE_PORT}"'/g' /usr/share/nginx/html/assets/config/config.json
 fi
 
-nginx -g daemon off;
+/usr/sbin/nginx -g daemon off;
