ac5e4cec4180adc66f6b8d5711193abe2bc37813
rwilson-ebi <rwilson@ebi.ac.uk>
2019-10-08 11:50:25 +0100

    Changes to obtain the gene symbol for the project-gene intention.

--- src/app/feature-modules/projects/model/intention-by-gene-attribute.ts
+++ src/app/feature-modules/projects/model/intention-by-gene-attribute.ts
@@ -1,6 +1,6 @@
 export class IntentionByGeneAttribute {
-    mgi_accession_id: string;
-    marker_symbol: string;
-    allele_type_name: string;
-    specie_name: string;
+    mgiAccessionIdValue: string;
+    symbol: string;
+    alleleTypeName: string;
+    speciesName: string;
 }
