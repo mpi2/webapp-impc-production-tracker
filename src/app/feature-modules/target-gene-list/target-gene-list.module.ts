2d72240d13a65736167ec49c979fff4649ccf098
Mauricio Martinez <mauroz77@gmail.com>
2019-11-02 13:49:40 +0000

    Fix lint errors.

--- src/app/feature-modules/target-gene-list/target-gene-list.module.ts
+++ src/app/feature-modules/target-gene-list/target-gene-list.module.ts
@@ -3,11 +3,9 @@ import { NgModule } from '@angular/core';
 import { TargetGeneListRoutingModule } from './target-gene-list-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { ListManagementComponent } from './components/list-management/list-management.component';
-import { ListsImporterComponent } from './components/lists-importer/lists-importer.component';
-
 
 @NgModule({
-  declarations: [ListManagementComponent, ListsImporterComponent],
+  declarations: [ListManagementComponent],
   imports: [
     TargetGeneListRoutingModule,
     SharedModule
