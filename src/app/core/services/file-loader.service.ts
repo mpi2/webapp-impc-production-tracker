b5552b1bb081b35f88e2e6c78f6d719b04dbc9a1
Mauricio Martinez <mauroz77@gmail.com>
2019-11-07 17:13:15 +0000

    Add export functionality to gene lists.

--- src/app/core/services/file-loader.service.ts
+++ src/app/core/services/file-loader.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { Papa } from 'ngx-papaparse';
 
 @Injectable({
     providedIn: 'root'
 })
 export class FileLoaderService {
+
+    constructor(private papa: Papa) {}
 
     getRecordsByColumn(records: any[], column) {
         const header: string[] = records[0];
@@ -16,5 +19,10 @@ export class FileLoaderService {
         console.log('index:', index);
 
         return data.map(x => x[index]);
+    }
+
+    public jsonToCsv(json): string {
+        console.log('json==>', json);
+        return this.papa.unparse(json);
     }
 }
