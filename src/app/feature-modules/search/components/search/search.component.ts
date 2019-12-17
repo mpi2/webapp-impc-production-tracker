34d1f4272615d00c404ccacdd6bb171fe4594701
Mauricio Martinez <mauroz77@gmail.com>
2019-12-17 09:26:37 +0000

    Show inputs when searching by file

--- src/app/feature-modules/search/components/search/search.component.ts
+++ src/app/feature-modules/search/components/search/search.component.ts
@@ -1,21 +1,14 @@
 import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
-import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
 import { ConfigurationData, ConfigurationDataService, LoggedUserService } from 'src/app/core';
 import { MatPaginator, MatSort, MatDialog, MatSidenav } from '@angular/material';
 import { SearchService, Search } from '../..';
 import { SearchResult } from '../../model/search.result';
-import { InformativeDialogComponent } from 'src/app/shared/components/informative-dialog/informative-dialog.component';
 import { ProjectIntention } from 'src/app/model/bio/project-intention';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, share, startWith } from 'rxjs/operators';
 import { FilterDefinition } from 'src/app/feature-modules/filters/model/filter-definition';
 import { FilterService } from 'src/app/feature-modules/filters/services/filter.service';
-
-class CheckboxElement implements NamedValue {
-  name: string;
-  isSelected: boolean;
-}
 
 @Component({
   selector: 'app-search',
@@ -29,30 +22,17 @@ export class SearchComponent implements OnInit, AfterViewInit {
   @ViewChild('drawer', { static: false }) drawer: MatSidenav;
 
   selectedSearchType = 'gene';
-
   inputSearchDefinition: any = undefined;
-
   dataSource: SearchResult[];
-
   displayedColumns: string[] = [];
-  selectAllWorkUnits = true;
-  panelOpenState = false;
-  searchForm: FormGroup;
-  searchControl = new FormControl();
   page: any = {};
-  workUnits: CheckboxElement[] = [];
-  workGroups: CheckboxElement[] = [];
   searchTypes: string[] = ['gene'];
-  masterSelected: boolean;
-  checkedList: any;
-  myTextarea: string;
   configurationData: ConfigurationData;
   filterVisible = false;
   filters: FilterDefinition[];
-
+  inputsByText: string[] = [];
   error;
   isLoading = true;
-  isRateLimitReached = false;
 
   isHandset$: Observable<boolean> = this.breakpointObserver
     .observe(Breakpoints.Handset)
@@ -136,9 +116,16 @@ export class SearchComponent implements OnInit, AfterViewInit {
     /* tslint:disable:no-string-literal */
     this.dataSource = data['results'];
     this.dataSource.map(x => this.buildSearchResultComments(x));
+    this.updateInputIfSearchedByFile();
     this.refreshVisibleColumns();
     this.page = data['page'];
     /* tslint:enable:no-string-literal */
+  }
+
+  private updateInputIfSearchedByFile() {
+    if (this.getSearchDefinitionType() === 'file') {
+      this.inputsByText = this.dataSource.map(x => x.input);
+    }
   }
 
   buildSearchResultComments(searchResult: SearchResult): void {
@@ -164,6 +151,14 @@ export class SearchComponent implements OnInit, AfterViewInit {
     return this.selectedSearchType;
   }
 
+  private getSearchDefinitionType(): string {
+    let type = undefined;
+    if (this.inputSearchDefinition) {
+      type = this.inputSearchDefinition.type;
+    }
+    return type;
+  }
+
   getGeneSymbolsAsArray(): string[] {
     if (this.inputSearchDefinition) {
       if (this.inputSearchDefinition.type === 'text') {
@@ -173,77 +168,11 @@ export class SearchComponent implements OnInit, AfterViewInit {
           geneSymbols.map(x => x.trim());
           return geneSymbols;
         }
+      } else {
+        return this.inputsByText;
       }
     }
     return [];
-  }
-
-  checkUncheckWorkUnits(e) {
-    for (const workUnit of this.workUnits) {
-      const selector = '#' + this.getIdFromWorkUnitName(workUnit.name);
-      const htmlElement = document.querySelector(selector) as HTMLInputElement;
-      htmlElement.checked = e.target.checked;
-      workUnit.isSelected = e.target.checked;
-    }
-  }
-
-  checkUncheckWorkGroups(e) {
-    for (const workGroup of this.workGroups) {
-      const selector = '#' + this.getIdFromWorkGroupName(workGroup.name);
-      const htmlElement = document.querySelector(selector) as HTMLInputElement;
-      htmlElement.checked = e.target.checked;
-      workGroup.isSelected = e.target.checked;
-    }
-  }
-
-  onCheckedWorkUnitElement(element: any) {
-    const isSelected = !element.isSelected;
-    element.isSelected = isSelected;
-    const workUnitSelectAll = document.querySelector('#workUnitsSelectAll') as HTMLInputElement;
-    workUnitSelectAll.checked = this.workUnits.filter(x => x.isSelected).length === this.workUnits.length;
-
-    return isSelected;
-  }
-
-  onCheckedWorkGroupElement(element: any) {
-    const isSelected = !element.isSelected;
-    element.isSelected = isSelected;
-    const workGroupSelectAll = document.querySelector('#workGroupsSelectAll') as HTMLInputElement;
-    workGroupSelectAll.checked = this.workGroups.filter(x => x.isSelected).length === this.workGroups.length;
-
-    return isSelected;
-  }
-
-  private validSearch(): boolean {
-    let isValid = true;
-    const input = this.getGeneSymbolsAsArray();
-    if (input.length > 0) {
-      if (!this.selectedSearchType) {
-        isValid = false;
-        const dialogRef = this.dialog.open(InformativeDialogComponent, {
-          width: '250px',
-          data: {
-            title: 'Please select a search type',
-            text: 'As you have defined an input for the search (' + input + '), you need to stablish a search type.'
-          }
-        });
-
-        dialogRef.afterClosed().subscribe(result => {
-        });
-      } else {
-        isValid = true;
-      }
-    }
-
-    return isValid;
-  }
-
-  getIdFromWorkUnitName(workUnitName: string) {
-    return 'workUnit_' + workUnitName.replace(/\W/g, '_');
-  }
-
-  getIdFromWorkGroupName(workGroupName: string) {
-    return 'workGroup_' + workGroupName.replace(/\W/g, '_');
   }
 
   getTargetText(projectIntention: ProjectIntention): string {
@@ -256,5 +185,4 @@ export class SearchComponent implements OnInit, AfterViewInit {
       return text;
     }
   }
-
 }
