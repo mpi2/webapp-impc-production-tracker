7274e5a6989c322a636c25063c5b4a2e26f0f5b8
Mauricio Martinez <mauroz77@gmail.com>
2020-07-20 18:46:46 +0100

    Update Project detail view
    -Second part implementation of project sequences: expandable content for intention sequence information

--- src/app/feature-modules/intentions/components/intention-list/intention-list.component.ts
+++ src/app/feature-modules/intentions/components/intention-list/intention-list.component.ts
@@ -2,11 +2,19 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ProjectIntention } from 'src/app/model';
 import { MatDialog } from '@angular/material/dialog';
 import { SequencesIntentionsDialogComponent } from '../sequences-intentions-dialog/sequences-intentions-dialog.component';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-intention-list',
   templateUrl: './intention-list.component.html',
-  styleUrls: ['./intention-list.component.css']
+  styleUrls: ['./intention-list.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 
 })
 export class IntentionListComponent implements OnInit {
@@ -20,7 +28,6 @@ export class IntentionListComponent implements OnInit {
   constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
-
   }
 
   openDialog(projectIntention: ProjectIntention): void {
