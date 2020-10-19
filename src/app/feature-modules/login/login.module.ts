ab2cd426cd6f435ac673f75bf85406a27fedf36f
albags <gomezsegura.alba@gmail.com>
2020-10-19 13:46:51 +0100

    Reset password functionality added.

--- src/app/feature-modules/login/login.module.ts
+++ src/app/feature-modules/login/login.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { LoginComponent } from './login/login.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { LoginRoutingModule } from './login-routing.module';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, ForgotPasswordComponent],
   imports: [
     SharedModule,
     LoginRoutingModule
